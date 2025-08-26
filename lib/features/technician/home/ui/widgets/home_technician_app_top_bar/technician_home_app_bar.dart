fbe58ee4230ba402bef33a0766eaee0ea7507fe8
Omar Hossam Fathey <153639313+OmarHossam666@users.noreply.github.com>
2025-08-26 18:51:31 +0300

    refactor(technician_home_app_bar): remove unused imports to clean up code

--- lib/features/technician/home/ui/widgets/home_technician_app_top_bar/technician_home_app_bar.dart
+++ lib/features/technician/home/ui/widgets/home_technician_app_top_bar/technician_home_app_bar.dart
@@ -1,10 +1,7 @@
-import 'package:fixly/core/constants/app_assets.dart';
-import 'package:fixly/core/constants/app_colors.dart';
 import 'package:fixly/core/helpers/spacing.dart';
 import 'package:fixly/core/widgets/surface_dark.dart';
 import 'package:fixly/core/widgets/theme_toggle_button.dart';
 import 'package:fixly/features/technician/home/ui/widgets/home_technician_app_top_bar/subwidgets/logo_and_title.dart';
-import 'package:fixly/features/technician/home/ui/widgets/home_technician_app_top_bar/subwidgets/notification_with_badge.dart';
 import 'package:fixly/features/technician/home/ui/widgets/home_technician_app_top_bar/subwidgets/status_online_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
